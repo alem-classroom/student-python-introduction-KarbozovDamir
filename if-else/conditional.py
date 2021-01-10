170468ba502f83d8d711af41a64704330ee51e0e
Karbozov Damir <damirkap89@mail.ru>
2021-01-11 03:01:56 +0600

    Initial commit

--- if-else/conditional.py
+++ if-else/conditional.py
@@ -1,16 +1,40 @@
 import random
 
 def is_positive(num):
-    # return true if num is positive, otherwise return false
+    if num > 0:
+        print(True)
+    else:
+        print(False)
+
+
+is_positive(1) # return true if num is positive, otherwise return false
 
 def is_even(num):
-    # return true if num is even, otherwise return false
+    if num % 2 == 0:
+        print(True)
+    else:
+        print(False)
+
+
+is_even(2) # return true if num is even, otherwise return false
 
 
 def is_positive_and_even(num):
-    # return true if num is positive and even, otherwise return false
+    if num > 0 and num % 2 ==0:
+        print(True)
+    else:
+        print(False)
+
+
+is_ positive_or_even(4) # return true if num is positive and even, otherwise return false
 
 
 def is_positive_or_even(num):
-    # return true if num is positive or even, otherwise return false
+    if num > 0 or num % 2 == 0:
+        print(True)
+    else:
+        print(False)
 
+
+is_positive_or_even(4)
+    # return true if num is positive or even, otherwise return false
