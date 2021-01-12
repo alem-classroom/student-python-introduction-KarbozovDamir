5325be8ec1b07bb4c85325dbe534d624ddce5c50
Karbozov Damir <damirkap89@mail.ru>
2021-01-12 12:55:15 +0600

    changed if-else

--- if-else/conditional.py
+++ if-else/conditional.py
@@ -1,61 +1,18 @@
 def is_positive(num):
-    if num > 0:
-        print(True)
-    else:
-        print(False)
+    # return true if num is positive, otherwise return false
+    return num > 0
 
-
-<<<<<<< HEAD
-is_positive(1)
-
-=======
-is_positive(1) # return true if num is positive, otherwise return false
->>>>>>> 170468ba502f83d8d711af41a64704330ee51e0e
 
 def is_even(num):
-    if num % 2 == 0:
-        print(True)
-    else:
-        print(False)
-
-
-<<<<<<< HEAD
-is_even(2)
+    # return true if num is even, otherwise return false
+    return num % 2 == 0
 
 
 def is_positive_and_even(num):
-    if num > 0 and num % 2 == 0:
-=======
-is_even(2) # return true if num is even, otherwise return false
-
-
-def is_positive_and_even(num):
-    if num > 0 and num % 2 ==0:
->>>>>>> 170468ba502f83d8d711af41a64704330ee51e0e
-        print(True)
-    else:
-        print(False)
-
-
-<<<<<<< HEAD
-is_positive_and_even(4)
-=======
-is_positive_or_even(4) # return true if num is positive and even, otherwise return false
->>>>>>> 170468ba502f83d8d711af41a64704330ee51e0e
+    # return true if num is positive and even, otherwise return false
+    return num > 0 and num % 2 == 0
 
 
 def is_positive_or_even(num):
-    if num > 0 or num % 2 == 0:
-        print(True)
-    else:
-        print(False)
-<<<<<<< HEAD
-
-
-is_positive_or_even(4)
-=======
-
-
-is_positive_or_even(4)
     # return true if num is positive or even, otherwise return false
->>>>>>> 170468ba502f83d8d711af41a64704330ee51e0e
+    return num > 0 or num % 2 == 0
