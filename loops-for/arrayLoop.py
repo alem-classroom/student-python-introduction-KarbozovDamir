61e159ab1f2271cf66ff4120b3158c5bda802d42
Karbozov Damir <damirkap89@mail.ru>
2021-01-24 11:07:47 +0600

    first commit

--- loops-for/arrayLoop.py
+++ loops-for/arrayLoop.py
@@ -1,2 +1,4 @@
 def insert_squares(arr, num):
-    # add square of numbers from 1 to num to the list named arr and return list
+    for i in range(1, num + 1):
+        arr.append(i * i)
+    return arr
