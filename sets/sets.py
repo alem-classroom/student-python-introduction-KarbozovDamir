d2421481c6d8e6732766b74eb6fa2afa8295ae8f
Damir K <damirkap89@gmail.com>
2021-02-02 12:07:25 +0600

    first commit

--- sets/sets.py
+++ sets/sets.py
@@ -1,18 +1,36 @@
 def size_of_set(set):
-    # return size of set  
+    return len(set)
+    # return size of set 
+     
 def is_elem_in_set(set, elem):
-    # return true if elem exists in set, false otherwise
+    if elem in set: 
+        return True
+
+    else:
+        return False
+# return true if elem exists in set, false otherwise
 
 def are_sets_equal(first_set, second_set):
+    if first_set == second_set: 
+        return True
+        
+    else:
+        return False
     # return true if sets have the same elements inside, otherwise false
 
 def add_elem_to_set(set, elem):
+    set.add(elem)
+    return set
     # add elem to the set if elem does not exist in set, and return the set
     # if elem exists in set, return set
 
 def remove_elem_if_exists(set, elem):
+    set.discard(elem)
+    return set
     # remove elem from set if it exists, and return the set
 
 def delete_first_element(set):
+    set.pop()
+    return set
     # delete first elemenent of set
     
