9fe16f0eb4cb2020e22023eb360cc166cd284dce
Damir K <damirkap89@gmail.com>
2021-02-02 17:19:06 +0600

    initial commit

--- variables-and-types/calculator.py
+++ variables-and-types/calculator.py
@@ -4,14 +4,14 @@ def add(first, second):
 
 def sub(first, second):
     # return subtraction of first and second arguments
-
+    return first - second
 def multiply(first, second):
     # return multiplicatiom of first and second arguments
-
+    return first * second
 def div_with_floor(first, second):
     # return division with floor of first and second arguments
-
+    return first // second
 
 def div_without_floor(first, second):
     # return division without floor of first and second arguments
-
+    return first / second
