0d02ea42577aa1b470a5602cec8d1c14d22c18cf
Damir K <damirkap89@gmail.com>
2021-02-02 11:44:25 +0600

    firsrt commit

--- lists/lists.py
+++ lists/lists.py
@@ -1,18 +1,29 @@
 def size_of_list(list):
+    return len(list)
     # return size of list
 
 def add_elem_to_list(list, elem):
+    list.append(elem)
+    return list
     # add elem to list and return the list
 
 def delete_elem_from_list(list, index = -1):
+    if index != -1 and len(list) > index:
+        return list.pop(index)
+
+    else:
+        return list.pop()
     # delete element from list, such that its index is index
     # if index is invalid, return empty list
 
 def count_elements_in_list(list, x):
+    return list.count(x)
     # count elements in the list that are equal to x and return the count
 
 def sort_list(list):
+    return list.sort()
     # return sorted list
 
 def reverse(list):
+    return list.reverse()
     # return reversed list
