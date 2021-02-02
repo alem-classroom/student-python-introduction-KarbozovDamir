4eb4aff6bd978f4e85bb8242f5c08ee7ada65517
Damir K <damirkap89@gmail.com>
2021-02-03 00:00:06 +0600

    changed code

--- variables-and-types/strings.py
+++ variables-and-types/strings.py
@@ -1,10 +1,13 @@
 def size_of_str(str):
     # return size of string
+    return len(str)
 
 def concat_strings(first, second):
     # return concatination of first and second strings
+    return (first + second)
 
 def duplicate_string(str, copy):
+    return (str*3)
     # return new string which is copy of str copy times
     # example -> duplicate_string('s', 2) == 'ss'
 
