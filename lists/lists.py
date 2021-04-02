e71859fb4e109a0f2d63d3a0ac33653181b4bcd1
Damir K <damirkap89@gmail.com>
2021-04-02 18:33:05 +0600

    changed func rev, func sort

--- lists/lists.py
+++ lists/lists.py
@@ -7,23 +7,22 @@ def add_elem_to_list(list, elem):
     return list
     # add elem to list and return the list
 
-def delete_elem_from_list(list, index = -1):
-    if index != -1 and len(list) > index:
-        return list.pop(index)
-
-    else:
-        return list.pop()
+def delete_elem_from_list(list, index = -1): 
+    return list.pop(index) 
+  
     # delete element from list, such that its index is index
     # if index is invalid, return empty list
+    
 
 def count_elements_in_list(list, x):
     return list.count(x)
     # count elements in the list that are equal to x and return the count
 
 def sort_list(list):
-    return list.sort()
+    list = list.sort()
+    return list
     # return sorted list
 
 def reverse(list):
-    return list.reverse()
+    list = list.reverse()
     # return reversed list
